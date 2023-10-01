cde8f17563c22eb0b4b64044b8e524df278a829c
guinfinityfnd <microsoftcompany1999@gmail.com>
2023-10-01 09:05:32 -0700

    first_release_for_only_me

---
+++ js.js
@@ -0,0 +1,21 @@
+const arrow_btn = document.querySelector(".greaterthan");
+const sideBar = document.querySelector(".sidebar");
+const my_h2 = document.querySelector(".my_h2");
+const lessThan_arrow = document.querySelector(".lessthan");
+
+arrow_btn.addEventListener("click",()=>{
+    arrow_btn.style.visibility = "hidden";
+    sideBar.style.minWidth = "50%" ;
+    my_h2.style.width = "57px";
+    lessThan_arrow.style.display = "block";
+    sideBar.classList.add("t_out");
+    sideBar.classList.remove("t_in");
+});
+
+lessThan_arrow.addEventListener("click",()=>{
+    arrow_btn.style.visibility = "visible";
+    my_h2.style.width = "185px";
+    sideBar.classList.remove("t_out");
+    sideBar.classList.add("t_in");
+    lessThan_arrow.style.display = "none";
+});
